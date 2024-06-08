91fc165bc773e5276ffd22905044fd43316b8cee
Sofia <msofia.lima@alu.ufc.br>
2024-06-08 15:13:42 -0300

    changed paths to urls

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -1,6 +1,6 @@
 export const images = {
     logo: 'src/assets/logo.png',
-    next: 'src/assets/next-page.png',
-    prev: 'src/assets/previous-page.png',
-    plus: 'src/assets/plus.png',
+    next: 'https://img.icons8.com/windows/32/circled-chevron-right.png',
+    prev: 'https://img.icons8.com/windows/32/circled-chevron-left.png',
+    plus: 'https://img.icons8.com/windows/32/plus.png',
 };
